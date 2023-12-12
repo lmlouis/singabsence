53d0efdcada199e8ce329c991f690224d61f10be
Louis <louiswora1@gmail.com>
2023-12-12 03:03:56 +0100

    Ajout des rôles employé, reponsable, directeur et administrateur

--- src/main/java/com/sing/singabsence/security/AuthoritiesConstants.java
+++ src/main/java/com/sing/singabsence/security/AuthoritiesConstants.java
@@ -5,6 +5,12 @@ package com.sing.singabsence.security;
  */
 public final class AuthoritiesConstants {
 
+    // Ajout des rôles employé, reponsable, directeur et administrateur
+    public static final String EMPLOYEE = "ROLE_EMPLOYEE";
+    public static final String MANAGER = "ROLE_MANAGER";
+    public static final String DIRECTOR = "ROLE_DIRECTOR";
+    public static final String ADMINISTRATOR = "ROLE_ADMINISTRATOR";
+
     public static final String ADMIN = "ROLE_ADMIN";
 
     public static final String USER = "ROLE_USER";
